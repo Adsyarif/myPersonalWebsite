648e348d50c8bceb0e08857480a31180f7096e90
Adrisa <adrisansyarif@gmail.com>
2023-11-01 13:14:56 +0700

    add minimum change themes, add laptop responsive, change several layout flex to grid

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,71 @@
 import data from "./../puns.json" assert { type: "json" };
 
+// select pun attribut
 let punAtribut = document.querySelector(".pun");
+
+// select modal atribut
 let firstModal = document.getElementsByClassName("pop-up")[0];
 let secondModal = document.getElementsByClassName("pop-up")[1];
 let thirdModal = document.getElementsByClassName("pop-up")[2];
 let modalBackGround = document.getElementById("card-form-modal");
+
+// select themes atribut
+let containerToggle = document.getElementById("container-toggle");
+let btnDay = document.querySelector("#container-day i");
+let btnNight = document.querySelector("#container-night i");
+let containerDay = document.getElementById("container-day");
+
+// select input form atribut
 let inputForm = document.querySelectorAll(".input");
 let userInput = [];
+
+// makes real time day counter
 const timeElapsed = Date.now();
 const todayDate = new Date(timeElapsed);
 document.getElementById("today").innerHTML = todayDate.toDateString();
 
+// event pop up form
 document.getElementById("modal-btn").addEventListener("click", () => {
   firstModal.classList.remove("hidden");
   firstModal.classList.add("active");
   modalBackGround.classList.add("block-content");
 });
 
+// event submit form button
 document.getElementById("submit").addEventListener("click", () => {
   countInput();
   checkedInput();
 });
 
-document.getElementById("dismiss-failed").addEventListener("click", () => {
-  thirdModal.classList.remove("active");
-});
-
+// close form button
 document.getElementById("close-btn").addEventListener("click", () => {
   firstModal.classList.remove("active");
   modalBackGround.classList.remove("block-content");
 });
 
+// failed event popup submit form button
+document.getElementById("dismiss-failed").addEventListener("click", () => {
+  thirdModal.classList.remove("active");
+});
+
+// success event popup submit form button
 document.getElementById("dismiss-popup").addEventListener("click", () => {
   secondModal.classList.remove("active");
   modalBackGround.classList.remove("block-content");
 });
 
+// pun generator
 punAtribut.addEventListener("click", () => {
   punGenerator();
   punOnClick();
+});
+
+document.getElementById("btn-themes-day").addEventListener("click", () => {
+  togleButtonDay();
+});
+
+document.getElementById("btn-themes-night").addEventListener("click", () => {
+  togleButtonNight();
 });
 
 const checkedInput = () => {
@@ -96,4 +123,33 @@ const punOnClick = () => {
   setTimeout(() => {
     punAtribut.classList.remove("animated-pun");
   }, 600);
+};
+
+const togleButtonNight = () => {
+  containerToggle.classList.add("themes-togle-night");
+  btnDay.classList.add("sun-config-night");
+  btnNight.classList.add("moon-config-night");
+  containerDay.classList.add("themes-day-night");
+  document.querySelector("body").classList.add("themes-day-night");
+
+  containerToggle.classList.remove("themes-togle-day");
+  btnDay.classList.remove("sun-config-day");
+  btnNight.classList.remove("moon-config-day");
+  containerDay.classList.remove("themes-day-day");
+  document.querySelector("body").classList.remove("themes-day-day");
+};
+
+const togleButtonDay = () => {
+  containerToggle.classList.add("themes-togle-day");
+  btnDay.classList.add("sun-config-day");
+  btnNight.classList.add("moon-config-day");
+  containerDay.classList.add("themes-day-day");
+  document.querySelector("body").classList.add("themes-day-day");
+
+  containerToggle.classList.remove("themes-togle-night");
+  btnDay.classList.remove("sun-config-night");
+  btnNight.classList.remove("moon-config-night");
+  containerDay.classList.remove("themes-day-night");
+
+  document.querySelector("body").classList.remove("themes-day-night");
 };
